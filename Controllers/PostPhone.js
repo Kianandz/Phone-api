7aff6e201da6947599b3ee0ea6e82e4d15ce93a4
Kianandz <115959488+Kianandz@users.noreply.github.com>
2023-09-03 21:17:17 +0700

    Update PostPhone.js

--- Controllers/PostPhone.js
+++ Controllers/PostPhone.js
@@ -2,8 +2,6 @@ const db = require("../db");
 
 const PostPhone = (req, res) => {
   const { name, brands, phone_image } = req.body;
-
-  // Memasukkan data baru ke dalam tabel "brands"
   db.query(
     "INSERT INTO phone (name, brands, phone_image) VALUES (?, ?, ?)",
     [name, brands, phone_image],
@@ -14,7 +12,6 @@ const PostPhone = (req, res) => {
           .status(500)
           .send("Terjadi kesalahan dalam menambahkan data ke database");
       } else {
-        // Mengirim respons bahwa data telah berhasil ditambahkan
         res.status(201).send("Data telah berhasil ditambahkan");
       }
     }
